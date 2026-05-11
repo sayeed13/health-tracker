468119e78e1991bd2a9e0ea67b43ec2bbff414e9
Sayeed <sksayed13@gmail.com>
2026-05-11 20:09:22 +0530

    seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,8 +18,8 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         $this->call([
-            //UserSeeder::class,
-            //TaskDefinitionSeeder::class,
+            UserSeeder::class,
+            TaskDefinitionSeeder::class,
         ]);
     }
 }
